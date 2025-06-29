15b6b26d9fe94a36e4979de34b7facd8c2040bba
rizzsv <rizq.syafriano@gmail.com>
2025-06-29 22:10:21 +0700

    fix: add logrequest at category

--- src/bin/category/category.controller.ts
+++ src/bin/category/category.controller.ts
@@ -3,12 +3,15 @@ import { CustomRequest } from "../../config/custom.config";
 import { createCategory } from "./category.model";
 import { CategoryService } from "./category.service";
 import { Wrapper } from "../../utils/wrapper.utils";
+import { logRequest } from "../../helper/logger.request";
 
 
 export class categoryController {
     static async createCategory(req: CustomRequest, res: Response, next: NextFunction): Promise<void> {
         try {
             const request: createCategory = req.body as createCategory
+
+            await logRequest(req, `POST /category/create` + JSON.stringify(request));
 
             const response = await CategoryService.createCategory(request)
             Wrapper.success(res, true, response, 'Succes Create Category', 200)
